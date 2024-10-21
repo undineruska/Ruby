e04726789e6ab730dd613a7990910331bec35088
undineruska <142745642+undineruska@users.noreply.github.com>
2024-10-21 15:00:46 +0300

    Update 99_bottles_of_beer.rb

--- 99_bottles_of_beer.rb
+++ 99_bottles_of_beer.rb
@@ -1,5 +1,14 @@
-99.downto (1) do |num|
+# :)
+# Start at 99 and count down to 1
+99.downto(1) do |num|
+  
+  # Print the current number of bottles in the song
   puts "#{num} bottles of beer on the wall. #{num} bottles of beer,"
+
+  # Instructions to take one bottle down and pass it around
   puts "take one down, pass it around,"
+
+  # Print the remaining bottles after taking one down
+  # Special case: if num is 1, print "no more bottles" instead of 0
   puts "#{num - 1} bottles of beer on the wall!"
 end
