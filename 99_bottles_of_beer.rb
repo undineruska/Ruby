25ece137aa30ef0075b137987018c517e41b9ddc
undineruska <142745642+undineruska@users.noreply.github.com>
2024-10-21 14:42:18 +0300

    Create 99_bottles_of_beer.rb

---
+++ 99_bottles_of_beer.rb
@@ -0,0 +1,5 @@
+99.downto (1) do |num|
+  puts "#{num} bottles of beer on the wall. #{num} bottles of beer,"
+  puts "take one down, pass it around,"
+  puts "#{num - 1} bottles of beer on the wall!"
+end
